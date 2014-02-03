b622f70bff78bf77402446aa8a2b6e4e8d6ec507
Maartje de Jonge <maartjedejonge@gmail.com>
2014-02-03 09:41:23 -0500

    User story 6 finished: deletion of bidconventions via context menu or delete button

--- js/viewmediators/event-bindings.js
+++ js/viewmediators/event-bindings.js
@@ -1,14 +1,14 @@
 /**
  * Implements event handlers for user actions on the tree view.
  */
-define(["knockout", "jquery", "viewmediators/ui-common"], function(ko, $, moduleUI) {
+define(["knockout", "jquery", "viewmediators/ui-common", "jquery-ui"], function(ko, $, moduleUI) {
 
     //blur: save 'description' of bid conventions
     $(document).on('blur', '.description', function(event) {
         ko.contextFor(this).$root.saveToLocalStorage();
     });
 
-    //blur: save 'description' of bid conventions
+    //enter: save 'description' of bid conventions
     $(document).on('keyup', '.description', function(event) {
         event.stopPropagation();
         var keyCode = event.keyCode || event.which;
@@ -20,24 +20,53 @@ define(["knockout", "jquery", "viewmediators/ui-common"], function(ko, $, module
 
     $(document).on('keyup', 'body', function(event) {
         var keyCode = event.keyCode || event.which;
-        if (keyCode === moduleUI.keycodes.DELETE) {
+        if (keyCode === moduleUI.keycodes.DELETE && $(".selected:last").length > 0) {
+            var elem = $(".selected:last").get(0);
+            deleteBidConventions(ko.contextFor(this).$root, elem);
             event.stopPropagation();
             event.preventDefault();
-            var bidsystem = ko.contextFor(this).$root;
-            bidsystem.deleteSelection();
-            bidsystem.saveToLocalStorage();
-            console.log("TODO: show dialog for deleting selected bidconventions");
         }
     });
     
     $(".cm-delete").click(function(){
-        var bidsystem = ko.contextFor(this).$root;
-        bidsystem.deleteSelection();
-        bidsystem.saveToLocalStorage();
-        console.log("TODO: show dialog for deleting selected bidconventions");
-        //TODO: delete selected, or better: show DELETE dialog
+        event.stopPropagation();
+        deleteBidConventions(ko.contextFor(this).$root, this);
     });
-
+    
+    function deleteBidConventions(bidsystem, elem) {
+        $("#dialog-confirm-delete-bid-convention").dialog({
+            position: {
+                my: "left top",
+                at: "right bottom",
+                of: elem
+            },
+            show: {
+                effect: 'fadeIn',
+                duration: 500
+            },
+            hide: {
+                effect: 'fadeOut',
+                duration: 500
+            },
+            resizable: false,
+            height: 240,
+            width: 400,
+            modal: true,
+            buttons: {
+                "Ok": function() {
+                    $(this).dialog("close");
+                    bidsystem.deleteSelection();
+                    bidsystem.saveToLocalStorage();
+                    console.log("ok");
+                },
+                "Cancel": function() {
+                    $(this).dialog("close");
+                    console.log("cancel");
+                }
+            }
+        });
+    };
+    
     //click: select a bid convention
     $(document).on('mousedown', '.bidconvention', function(event) {
         ko.contextFor(this).$root.select(ko.contextFor(this).$data);
@@ -47,11 +76,14 @@ define(["knockout", "jquery", "viewmediators/ui-common"], function(ko, $, module
         return false;
     });
 
-    //click: deselect a bid convention
-    $(document).on('mousedown', 'body', function(event) {
-        console.log(this);
-        ko.contextFor(this).$root.clearSelection();
-    });
+    // $("#tree-view").on('mouseup', function(event) {
+    //     event.stopPropagation();
+    // });
+
+    // //click: deselect a bid convention
+    // $("body").on('mouseup', function(event) {
+    //     ko.contextFor(this).$root.clearSelection();
+    // });
 
     $(document).on('contextmenu', '.bidconvention', function(event) {
         event.stopPropagation();
