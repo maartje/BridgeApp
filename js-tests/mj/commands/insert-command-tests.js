c9882e53f7e7b1190e25bef12288f384331f62f8
maartje <maartjedejonge@gmail.com>
2014-04-22 12:34:01 +0200

    insert command plus unit tests

---
+++ js-tests/mj/commands/insert-command-tests.js
@@ -0,0 +1,77 @@
+define(function(require) {
+    var assert = require('chai').assert;
+    var baseCommandModule = require('mj/commands/base-command');
+    var insertCommandModule = require('mj/commands/insert-command');
+    var fakeViewStateManagerModule = require('../../fake-objects/fake-viewstate-manager');
+    var fakeNodeModule = require('../../fake-objects/fake-tree-node');
+
+    //system under test
+    var insertCommand;
+    
+    //mock and stub objects
+    var fakeViewstateManager;
+    var node_000;
+    var node_001;
+    
+    /*global setup, test, suite*/
+
+    setup(function() {
+        fakeViewstateManager = new fakeViewStateManagerModule.FakeViewStateManager();
+        fakeNodeModule.initializeTestData();
+        var baseCommand = new baseCommandModule.BaseCommand(fakeViewstateManager);
+        var fakeTreeNodeCollection = new fakeNodeModule.FakeTreeNodeCollection([]);
+        insertCommand = new insertCommandModule.InsertCommand(baseCommand, fakeTreeNodeCollection);
+
+        node_000 = new fakeNodeModule.FakeTreeNode("000", []);
+        node_001 = new fakeNodeModule.FakeTreeNode("000", []);
+        var treeNodeCollection = new fakeNodeModule.FakeTreeNodeCollection([node_000, node_001]);
+        insertCommand.setTreeNodeCollection(treeNodeCollection);
+
+    });
+
+
+    suite('InsertCommand', function() {
+        test('#execute() attachs the given set of nodes to the data structure', function() {
+            // act
+            insertCommand.execute();
+
+            // assert
+            assert.isTrue(node_000.isAttached);
+            assert.isTrue(node_001.isAttached);
+        });
+        
+        test('#execute() sets the focus of the viewstate on the inserted nodes', function() {
+            // act
+            insertCommand.execute();
+
+            // assert
+            assert.isTrue(node_000.hasFocus);
+            assert.isTrue(node_001.hasFocus);
+        });
+
+
+        test('#undoExecute() resets the viewstate', function() {
+            // arrange
+            var expectedViewState = fakeViewstateManager.getViewState();
+
+            // act
+            insertCommand.execute();
+            insertCommand.undoExecute();
+
+            // assert
+            assert.equal(fakeViewstateManager.getViewState(), expectedViewState);
+        });
+
+        test('#undoExecute() restores the data structure by attaching all detached nodes', function() {
+            // arrange
+
+            // act
+            insertCommand.execute();
+            insertCommand.undoExecute();
+
+            // assert
+            assert.isFalse(node_000.isAttached);
+            assert.isFalse(node_001.isAttached);
+        });
+    });
+});
---
+++ js/mj/commands/insert-command.js
@@ -0,0 +1,72 @@
+/**
+ * Represents the action to insert a given set of nodes 
+ * into the data structure.
+ * The nodes are inserted based on their parent pointer.
+ * Provides the functionality to undo the insert operation.
+ * Remark: used as a base class for other commands such as
+ * @CopyToCommand, @MoveToCommand, @CreateChildCommand and @CreateSiblingCommand 
+ */
+define(function(require, exports, module) {
+    var mixinModule = require("util/mixin");
+
+    /**
+     * Constructor for the InsertCommand
+     * @param baseCommand {BaseCommand}
+     * @param treeNodeCollection {TreeNodeCollection}
+     */
+    var InsertCommand = module.exports.InsertCommand = function(baseCommand, treeNodeCollection) {
+        mixinModule.MIXIN(baseCommand, this);
+        this._baseCommand = baseCommand;
+        this._nodes = treeNodeCollection.getNodes();
+    };
+
+    InsertCommand.prototype = function() {
+
+        /**
+         * Inserts a given set of nodes into the data structure;
+         * the nodes are inserted based on their parent pointer.
+         * Updates the viewstate so that the inserted nodes are focused.
+         */
+        var execute = function() {
+            //stores the viewstate to enable undo
+            this._baseCommand.execute();
+
+            //modifies the data structure
+            for (var i = 0; i < this._nodes.length; i++) {
+                var node = this._nodes[i];
+                node.attach();
+            }
+            
+            //updates the viewstate
+            this.getViewStateManager().setFocus(this._nodes);
+        };
+
+        /**
+         * Undo the insert operation by removing the inserted nodes from the data structure.
+         * Resets the viewstate so that it corresponds to the state before the insert operation.
+         */
+        var undoExecute = function() {
+            //resets the data structure
+            for (var i = 0; i < this._nodes.length; i++) {
+                var node = this._nodes[i];
+                node.detach();
+            }
+
+            //resets the viewstate
+            this._baseCommand.undoExecute();
+        };
+        
+        /**
+         * Sets the nodes that will be inserted
+         */
+        var setTreeNodeCollection = function(treeNodeCollection) {
+           this._nodes = treeNodeCollection.getNodes();
+     };
+
+        return {
+            setTreeNodeCollection : setTreeNodeCollection,
+            execute : execute,
+            undoExecute : undoExecute,
+        };
+    }();
+});
